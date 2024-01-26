49c8a29ae22382c6e3dc7a3671fb889e52cea12b
Gabriel Ballesteros <gabriel@coratronics.com>
2024-01-27 09:25:32 +1000

    all aspects of the table now seem to be working.

--- reelrack-fw/main/website/script.js
+++ reelrack-fw/main/website/script.js
@@ -56,7 +56,7 @@ fetch("http://your-server-url/api/reels")
   .catch((error) => console.error("Error:", error));
 
 // Mock data
-const data = [
+let data = [
   {
     id: 0,
     value: "Value 1",
@@ -148,39 +148,71 @@ function setupModal(button, modal, reelId) {
   // Get the input field for ID
   var idField = modal.querySelector("#id");
 
-  
+  // Get other input fields
+  var valueField = modal.querySelector("#value");
+  var packageField = modal.querySelector("#package");
+  var quantityField = modal.querySelector("#quantity");
+  var manufacturerField = modal.querySelector("#manufacturer");
+  var partNumberField = modal.querySelector("#part_number");
+  var skuField = modal.querySelector("#sku");
+  var compTypeField = modal.querySelector("#comp_type");
 
-  // When the user clicks the button, open the modal 
-  button.onclick = function() {
+  // When the user clicks the button, open the modal
+  button.onclick = function () {
     modal.style.display = "block";
-      // Check if reelId is a number
+    // Check if reelId is a number
     if (typeof reelId === "number") {
       // Set the reelId field as plain text read-only
       idField.setAttribute("readonly", true);
       idField.style.backgroundColor = "lightgray";
       idField.value = reelId;
+
+      var reelData = data.find(item => item.id === reelId);
+
+    if (reelData) {
+      // Set the reelId field as plain text read-only
+      idField.setAttribute("readonly", true);
+      idField.style.backgroundColor = "lightgray";
+      idField.value = reelData.id;
+
+      // Populate other fields using data
+      valueField.value = reelData.value;
+      packageField.value = reelData.package;
+      quantityField.value = reelData.quantity;
+      manufacturerField.value = reelData.manufacturer;
+      partNumberField.value = reelData.part_number;
+      skuField.value = reelData.sku;
+      compTypeField.value = reelData.comp_type;
+    } 
     } else {
       // Set the reelId field as editable
       idField.removeAttribute("readonly");
       idField.style.backgroundColor = "white";
       idField.value = "";
+
+      //flush all the other values
+      valueField.value = "";
+      packageField.value = "";
+      quantityField.value = "";
+      manufacturerField.value = "";
+      partNumberField.value = "";
+      skuField.value = "";
+      compTypeField.value = "";
     }
-  }
+  };
 
   // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
+  span.onclick = function () {
     modal.style.display = "none";
-  }
+  };
 
   // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
+  window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
-  }
+  };
 }
-
-
 
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("addRow");
@@ -253,7 +285,6 @@ function updateTable() {
           });
           td.appendChild(reelIDButton);
 
-          
           const editButton = document.createElement("button");
           editButton.textContent = "✎"; // Edit symbol
           editButton.style.marginLeft = "5px";
@@ -269,8 +300,6 @@ function updateTable() {
   table.appendChild(tbody);
 }
 
-
-
 // Initial table update
 updateTable();
 
@@ -282,59 +311,72 @@ document
   .getElementById("toggle-quantity")
   .addEventListener("change", updateTable);
 
-  document.getElementById('addRow').addEventListener('click', function() {
-    var newRowData = {
-      // Data for the new row goes here
-    };
-  
-    fetch('http://esp32-server/add-row', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newRowData)
-    });
+document.getElementById("addRow").addEventListener("click", function () {
+  var newRowData = {
+    // Data for the new row goes here
+  };
+
+  fetch("http://esp32-server/add-row", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newRowData),
   });
+});
 
-
-
-
-  // Handle form submission
-  document.getElementById('newRowForm').addEventListener('submit', function(event) {
+// Handle form submission
+document
+  .getElementById("newRowForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
     var newRowData = {
-      id: document.getElementById('id').value,
-      value: document.getElementById('value').value,
-      package: document.getElementById('package').value,
-      part_number: document.getElementById('part_number').value,
-      comp_type: document.getElementById('comp_type').value,
-      sku: document.getElementById('sku').value,
-      manufacturer: document.getElementById('manufacturer').value,
-      quantity: document.getElementById('quantity').value
+      id: Number(document.getElementById("id").value),
+      value: document.getElementById("value").value,
+      package: document.getElementById("package").value,
+      part_number: document.getElementById("part_number").value,
+      comp_type: document.getElementById("comp_type").value,
+      sku: document.getElementById("sku").value,
+      manufacturer: document.getElementById("manufacturer").value,
+      quantity: Number(document.getElementById("quantity").value),
     };
 
-    fetch('http://esp32-server/add-row', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newRowData)
-    }).then(function() {
-      // Close the modal
-      modal.style.display = "none";
+    // Find the corresponding object in the data array
+    var reelData = data.find(item => item.id === newRowData.id);
 
-      // Clear the form
-      document.getElementById('id').value = '';
-      document.getElementById('value').value = '';
-      document.getElementById('package').value = '';
-      document.getElementById('part_number').value = '';
-      document.getElementById('comp_type').value = '';
-      document.getElementById('sku').value = '';
-      document.getElementById('manufacturer').value = '';
-      document.getElementById('quantity').value = '';
+    if (reelData) {
+      // Update the object with the new values
+      Object.assign(reelData, newRowData);
+    } else {
+      // If the object doesn't exist, add a new one
+      data.push(newRowData);
+    }
 
-      // Update the table
-      updateTable();
-    });
+    //FIXME: add again when the server is ready
+    // fetch("http://esp32-server/add-row", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(newRowData),
+    // }).then(function () {
+    //   // Close the modal
+    //   modal.style.display = "none";
+
+    //   // Clear the form
+    //   document.getElementById("id").value = "";
+    //   document.getElementById("value").value = "";
+    //   document.getElementById("package").value = "";
+    //   document.getElementById("part_number").value = "";
+    //   document.getElementById("comp_type").value = "";
+    //   document.getElementById("sku").value = "";
+    //   document.getElementById("manufacturer").value = "";
+    //   document.getElementById("quantity").value = "";
+
+    //   // Update the table
+    //   updateTable();
+    // });
+    modal.style.display = "none";
+    updateTable();
   });
